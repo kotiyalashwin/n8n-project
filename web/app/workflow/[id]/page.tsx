1d87a643dc9d04a83c0f3eeb2e3b1edccb41379c
kotiyalashwin <ashwinkotiyal07@gmail.com>
2025-10-01 16:09:46 +0530

    deleted web

--- web/app/workflow/[id]/page.tsx
+++
@@ -1,8 +0,0 @@
-import { WorkFlowEditor } from "@/components/WorkFlowEditor";
-
-export default async function Page({ params }: { params: { id: string } }) {
-  const workFlowId = (await params).id;
-  console.log(workFlowId);
-
-  return <WorkFlowEditor workFlowId={workFlowId} />;
-}
--- web/costant.ts
+++
@@ -1,2 +0,0 @@
- export const BACKEN_URL=process.env.NEXT_PUBLIC_BACKEND_URL as string;
- export const WS_URL=process.env.NEXT_PUBLIC_WS_URL as string;
